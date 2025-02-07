620de2051c27edafb6235fbd4d27b70c7ac82f0a
ncaco <ncaco97@gmail.com>
2025-02-07 22:27:06 +0900

    sw.js change

--- push.js
+++ push.js
@@ -14,14 +14,7 @@ push.setVapidDetails(
 );
 
 
-let sub = {
-    endpoint: "https://fcm.googleapis.com/fcm/send/fyfgoQ4tKXg:APA91bFvK4o0Q0lxreUTB6RiG3onzuVoIh5dFG2dVoteypvMKx8monz-6QkYxZAUf3rOtnXqe-U45Vb06HBClLLnXlP5DKeR23y_PtsMbPcWL3DIYrvRbDLrfCLJogmJmrg_ji11LDOi",
-    expirationTime: null,
-    keys: {
-        p256dh: "BOtOcOAHUBn9ufSI464TzwkC_CJ-E9lde5NMyC1v5jnAZlMcKTP7K7xG-99cVLGytgmY3VQt4knMEKN9_FK0uL8",
-        auth: "mp6syM_aPZpodQI7GRp07A"
-    }
-}
+let sub = {endpoint:"https://fcm.googleapis.com/fcm/send/fyfgoQ4tKXg:APA91bFvK4o0Q0lxreUTB6RiG3onzuVoIh5dFG2dVoteypvMKx8monz-6QkYxZAUf3rOtnXqe-U45Vb06HBClLLnXlP5DKeR23y_PtsMbPcWL3DIYrvRbDLrfCLJogmJmrg_ji11LDOi",expirationTime:null,keys:{p256dh:"BOtOcOAHUBn9ufSI464TzwkC_CJ-E9lde5NMyC1v5jnAZlMcKTP7K7xG-99cVLGytgmY3VQt4knMEKN9_FK0uL8",auth:"mp6syM_aPZpodQI7GRp07A"}}
 
 
 push.sendNotification(sub, 'test meassge');
