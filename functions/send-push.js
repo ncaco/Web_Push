a3ef9bca86fb44d857553823b5498203961f0470
ncaco <ncaco97@gmail.com>
2025-02-09 21:29:08 +0900

    DIST_MKDIR_V3

--- functions/send-push.js
+++ functions/send-push.js
@@ -2,11 +2,16 @@ const admin = require('firebase-admin');
 
 // Firebase Admin 초기화
 if (!admin.apps.length) {
+    // private key 처리
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        : undefined;
+
     admin.initializeApp({
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+            privateKey: privateKey
         }),
         databaseURL: process.env.FIREBASE_DATABASE_URL
     });
